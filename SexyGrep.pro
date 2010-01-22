89372e64c268ba09a6fd5d304a8d9bfa8813c649
icebreaker <theicebreaker007@gmail.com>
2010-01-22 21:16:59 +0200

    initial commit

---
+++ SexyGrep.pro
@@ -0,0 +1,18 @@
+#-------------------------------------------------
+#
+# Project created by QtCreator 2010-01-22T21:15:43
+#
+#-------------------------------------------------
+
+QT       += core gui
+
+TARGET = SexyGrep
+TEMPLATE = app
+
+
+SOURCES += main.cpp\
+        mainwindow.cpp
+
+HEADERS  += mainwindow.h
+
+FORMS    += mainwindow.ui
---
+++ mainwindow.h
@@ -0,0 +1,25 @@
+/*$LICENSE$*/
+
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+
+namespace Ui {
+    class MainWindow;
+}
+
+class MainWindow : public QMainWindow {
+    Q_OBJECT
+public:
+    explicit MainWindow(QWidget *parent = 0);
+    ~MainWindow();
+
+protected:
+    void changeEvent(QEvent *e);
+
+private:
+    Ui::MainWindow *ui;
+};
+
+#endif // MAINWINDOW_H
